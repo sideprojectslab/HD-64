c3d1ead36cec1c0cdd978e1fd4984952bb3b82ba
Chuck-R <github@chuck.cloud>
2025-04-22 01:00:21 -0500

    Vertical adjustment, attempt 6
    Signed-off-by: Chuck-R <github@chuck.cloud>

--- development/firmware/vicii/vic_pkg.vhdl
+++ development/firmware/vicii/vic_pkg.vhdl
@@ -135,22 +135,6 @@ package vic_pkg is
 	-- NTSC with 64 lines
 	constant c_vic_h64_specs : t_vic_specs :=
 	(
-
---          | Video  | # of  | Visible | Cycles/ |  Visible
---   Type   | system | lines |  lines  |  line   | pixels/line
--- ---------+--------+-------+---------+---------+------------
--- 6567R56A | NTSC-M |  262  |   234   |   64    |    411
---  6567R8  | NTSC-M |  263  |   235   |   65    |    418
---   6569   |  PAL-B |  312  |   284   |   63    |    403
---
---          | First  |  Last  |              |   First    |   Last
---          | vblank | vblank | First X coo. |  visible   |  visible
---   Type   |  line  |  line  |  of a line   |   X coo.   |   X coo.
--- ---------+--------+--------+--------------+------------+-----------
--- 6567R56A |   13   |   40   |  412 ($19c)  | 488 ($1e8) | 388 ($184)
---  6567R8  |   13   |   40   |  412 ($19c)  | 489 ($1e9) | 396 ($18c)
---   6569   |  300   |   15   |  404 ($194)  | 480 ($1e0) | 380 ($17c)
-
 		tvic => vic_h64,
 
 		cycl => to_ppos(64),
@@ -163,8 +147,8 @@ package vic_pkg is
 		xlvc => to_ppos(21 + 319),
 
 		yref => to_ppos(0),
-		ynul => to_ppos(30),
-		yend => to_ppos(30 + 233),
+		ynul => to_ppos(34),
+		yend => to_ppos(34 + 233),
 		ylen => to_ppos(262),
 		yres => to_ppos(234),
 		yfvc => to_ppos(49), 
